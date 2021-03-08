c592b757c592b2e5ad301f42c627b6b76b526430
Robert Brown <brown.rjb75@gmail.com>
2021-03-07 19:48:09 -0700

    Modularized and moved static config vars to config.js

---
+++ config/config.js
@@ -0,0 +1,11 @@
+//=============================
+//System Configs
+//=============================
+
+module.exports = {
+    prefix: '!',
+    VerificationChannelId: '',
+    VerificationRoleName: 'Verified'
+};
+
+// to import this file use var config = require("./config/config.js")
